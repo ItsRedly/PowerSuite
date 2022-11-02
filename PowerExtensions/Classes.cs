2c3174b8be240cbc72d819f59ec3a335324d5a37
ItsRedly <alvesdinis74@gmail.com>
2022-11-02 18:36:47 +0000

    Initial commit

--- PowerExtensions/Classes.cs
+++ PowerExtensions/Classes.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
-using System.Drawing;
+using Wice;
+using Image = System.Drawing.Image;
+using Path = System.IO.Path;
 using Newtonsoft.Json;
 using System.Net;
 using System.Text;
@@ -302,166 +304,18 @@ namespace PowerExtensions
         }
     }
 
-    public enum ShowWindowCommands : int
-    {
-        Hide = 0,
-        Normal = 1,
-        ShowMinimized = 2,
-        Maximize = 3,
-        ShowMaximized = 3,
-        ShowNoActivate = 4,
-        Show = 5,
-        Minimize = 6,
-        ShowMinNoActive = 7,
-        ShowNA = 8,
-        Restore = 9,
-        ShowDefault = 10,
-        ForceMinimize = 11
-    }
-
-    public delegate IntPtr WndProcDelegate(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
-    public delegate IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
-
-    public enum ClassStyles : uint
-    {
-        ByteAlignClient = 0x1000,
-        ByteAlignWindow = 0x2000,
-        ClassDC = 0x40,
-        DoubleClicks = 0x8,
-        DropShadow = 0x20000,
-        GlobalClass = 0x4000,
-        HorizontalRedraw = 0x2,
-        NoClose = 0x200,
-        OwnDC = 0x20,
-        ParentDC = 0x80,
-        SaveBits = 0x800,
-        VerticalRedraw = 0x1
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct WNDCLASS
-    {
-        public ClassStyles style;
-        [MarshalAs(UnmanagedType.FunctionPtr)]
-        public WndProc lpfnWndProc;
-        public int cbClsExtra;
-        public int cbWndExtra;
-        public IntPtr hInstance;
-        public IntPtr hIcon;
-        public IntPtr hCursor;
-        public IntPtr hbrBackground;
-        [MarshalAs(UnmanagedType.LPTStr)]
-        public string lpszMenuName;
-        [MarshalAs(UnmanagedType.LPTStr)]
-        public string lpszClassName;
-    }
-
-    public struct POINT
-    {
-        public Int32 x;
-        public Int32 Y;
-    }
-
-    [StructLayout(LayoutKind.Sequential, Pack = 8)]
-    public struct MSG
-    {
-        public IntPtr hwnd;
-        public UInt32 message;
-        public UIntPtr wParam;
-        public UIntPtr lParam;
-        public UInt32 time;
-        public POINT pt;
-    }
-
-    public enum WindowStyles : uint
-    {
-        WS_BORDER = 0x800000,
-        WS_CAPTION = 0xc00000,
-        WS_CHILD = 0x40000000,
-        WS_CLIPCHILDREN = 0x2000000,
-        WS_CLIPSIBLINGS = 0x4000000,
-        WS_DISABLED = 0x8000000,
-        WS_DLGFRAME = 0x400000,
-        WS_GROUP = 0x20000,
-        WS_HSCROLL = 0x100000,
-        WS_MAXIMIZE = 0x1000000,
-        WS_MAXIMIZEBOX = 0x10000,
-        WS_MINIMIZE = 0x20000000,
-        WS_MINIMIZEBOX = 0x20000,
-        WS_OVERLAPPED = 0x0,
-        WS_OVERLAPPEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_SIZEFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
-        WS_POPUP = 0x80000000u,
-        WS_POPUPWINDOW = WS_POPUP | WS_BORDER | WS_SYSMENU,
-        WS_SIZEFRAME = 0x40000,
-        WS_SYSMENU = 0x80000,
-        WS_TABSTOP = 0x10000,
-        WS_VISIBLE = 0x10000000,
-        WS_VSCROLL = 0x200000
-    }
-
-    public enum WindowStylesEx : uint
-    {
-        WS_EX_ACCEPTFILES = 0x00000010,
-        WS_EX_APPWINDOW = 0x00040000,
-        WS_EX_CLIENTEDGE = 0x00000200,
-        WS_EX_COMPOSITED = 0x02000000,
-        WS_EX_CONTEXTHELP = 0x00000400,
-        WS_EX_CONTROLPARENT = 0x00010000,
-        WS_EX_DLGMODALFRAME = 0x00000001,
-        WS_EX_LAYERED = 0x00080000,
-        WS_EX_LAYOUTRTL = 0x00400000,
-        WS_EX_LEFT = 0x00000000,
-        WS_EX_LEFTSCROLLBAR = 0x00004000,
-        WS_EX_LTRREADING = 0x00000000,
-        WS_EX_MDICHILD = 0x00000040,
-        WS_EX_NOACTIVATE = 0x08000000,
-        WS_EX_NOINHERITLAYOUT = 0x00100000,
-        WS_EX_NOPARENTNOTIFY = 0x00000004,
-        WS_EX_NOREDIRECTIONBITMAP = 0x00200000,
-        WS_EX_OVERLAPPEDWINDOW = WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE,
-        WS_EX_PALETTEWINDOW = WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST,
-        WS_EX_RIGHT = 0x00001000,
-        WS_EX_RIGHTSCROLLBAR = 0x00000000,
-        WS_EX_RTLREADING = 0x00002000,
-        WS_EX_STATICEDGE = 0x00020000,
-        WS_EX_TOOLWINDOW = 0x00000080,
-        WS_EX_TOPMOST = 0x00000008,
-        WS_EX_TRANSPARENT = 0x00000020,
-        WS_EX_WINDOWEDGE = 0x00000100
-    }
-
     public class Window
     {
-        [DllImport("user32.dll")]
-        public static extern IntPtr DispatchMessage([In] ref MSG lpmsg);
-        [DllImport("user32.dll")]
-        public static extern bool TranslateMessage([In] ref MSG lpMsg);
-        [DllImport("user32.dll")]
-        public static extern sbyte GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
-        [DllImport("user32.dll", SetLastError = true, EntryPoint = "CreateWindowEx")]
-        public static extern IntPtr CreateWindowEx(WindowStylesEx dwExStyle, string lpClassName, string lpWindowName, WindowStyles dwStyle, int x, int y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommands nCmdShow);
-        public List<Control> Controls;
         public string Title;
-        IntPtr handle;
+        Window window;
         public Window(string title = "Untitled Window")
         {
             Title = title;
-            WNDCLASS wndClass = new WNDCLASS();
-            handle = CreateWindowEx(0, title, title, WindowStyles.WS_OVERLAPPEDWINDOW, -1, -1, -1, -1, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            window = new Window();
+            Task.Run(() => { window.Title = Title; });
         }
 
-        public void Show()
-        {
-            ShowWindow(handle, ShowWindowCommands.Normal);
-        }
-
-        public void Draw()
-        {
-            foreach (Control c in Controls) { c.Draw(this); }
-        }
+        public void Show() { window.Show(); }
     }
 
     public abstract class Control
