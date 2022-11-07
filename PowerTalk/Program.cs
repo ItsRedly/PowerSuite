ab8ad467266ffd6a673eb974a960594197f6f467
ItsRedly <alvesdinis74@gmail.com>
2022-11-07 16:45:28 +0000

    Sum changes

--- PowerTalk/Program.cs
+++ PowerTalk/Program.cs
@@ -4,14 +4,12 @@ using PowerAPI;
 namespace PowerTalk {
     public static class Program {
         public static void Main() {
-            Window window = new Window("PowerTalk");
+            Window window = new Window("PowerTalk", false, false, new(0, 0), new());
             window.BackgroundColor = Color.LightBlue;
-            window.Controls.Add(new TextControl(window.Title) { IsHCentered = true });
+            window.Controls.Add(new TextControl(window.Title) { IsCentered = true });
             window.Controls.Add(new TextControl("UR MUM!!!!") { Location = new Point(69, 69) });
-            window.Controls.Add(new TextControl("UR MUM!!!!") { Location = new Point(420, 420) });
+            window.Controls.Add(new TextControl("X") { Location = new Point(64, 64)});
             window.Show();
         }
     }
 }
-
-//
